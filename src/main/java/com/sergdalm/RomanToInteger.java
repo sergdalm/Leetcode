4a291a50a29035490704ff1ccb7e82aaac4b9e6b
Alex <sergdalm@gmail.com>
2022-06-12 10:05:20 +0300

    Added Roman to Integer solution

---
+++ src/main/java/com/sergdalm/RomanToInteger.java
@@ -0,0 +1,82 @@
+package com.sergdalm;
+
+import java.util.Map;
+
+/**
+ * No: 13
+ * Title: Roman to Integer
+ * Difficulty: Easy
+ * <p>
+ * Roman numerals are represented by seven different symbols:
+ * I, V, X, L, C, D and M.
+ * <p>
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * <p>
+ * For example, 2 is written as II in Roman numeral,
+ * just two ones added together. 12 is written as XII,
+ * which is simply X + II. The number 27 is written as XXVII,
+ * which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest
+ * from left to right. However, the numeral for four is not IIII.
+ * Instead, the number four is written as IV. Because the one
+ * is before the five we subtract it making four. The same principle
+ * applies to the number nine, which is written as IX. There are six
+ * instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ * <p>
+ * Constraints:
+ * 1 <= s.length <= 15
+ * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+ */
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("IX"));
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII")); // 58
+        System.out.println(romanToInt("MCMXCIV")); // 1994
+    }
+
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> romans = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000
+        );
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i != s.length() - 1) &&
+                    romans.get(s.charAt(i)) < romans.get(s.charAt(i + 1))) {
+                result += romans.get(s.charAt(i + 1)) - romans.get(s.charAt(i));
+                i++;
+            } else {
+                result += romans.get(s.charAt(i));
+            }
+
+        }
+        return result;
+    }
+
+}
